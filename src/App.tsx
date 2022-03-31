66aaf9c5bd1290546ce3c618d38d12e547529040
chadlada <chadlada@gmail.com>
2022-03-31 16:24:15 -0400

    Static implementation HTML start

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <h1>Minesweeper</h1>
+      <p>
+        <strong>Instructions:</strong>Choose difficulty level and may the odds
+        be ever in your favor!
+      </p>
+
+      <label>Difficulty:</label>
+
+      <select name="difficulty" id="difficulty">
+        <option value="">--Please choose an option--</option>
+        <option value="easy">Easy</option>
+        <option value="medium">Medium</option>
+        <option value="hard">Hard</option>
+      </select>
+    </div>
+  )
 }
