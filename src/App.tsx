3d6e29885f30be234a7524b7e7b55af3eaa4d50a
chadlada <chadlada@gmail.com>
2022-07-31 18:37:56 -0400

    Complete. UseEffect added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+type GameDifficulty = 0 | 1 | 2
 
 export function App() {
   const [game, setGame] = useState({
@@ -17,8 +19,32 @@ export function App() {
     mines: undefined,
   })
 
-  async function handleNewGame() {
-    const gameOptions = { difficulty: 0 }
+  const [difficulty, setDifficulty] = useState<GameDifficulty>(0)
+
+  useEffect(function () {
+    async function loadExistingGame() {
+      const existingGameId = localStorage.getItem('game-id')
+      const existingDifficulty = localStorage.getItem('game-difficulty')
+      console.log(existingGameId)
+
+      if (existingGameId && existingDifficulty) {
+        const response = await fetch(
+          `http://minesweeper-api.herokuapp.com/games/${existingGameId}`
+        )
+
+        if (response.ok) {
+          const gameJson = await response.json()
+
+          setGame(gameJson)
+          setDifficulty(Number(existingDifficulty) as GameDifficulty)
+        }
+      }
+    }
+    loadExistingGame()
+  }, [])
+
+  async function handleNewGameButtonClick(newGameDifficulty: 0 | 1 | 2) {
+    const gameOptions = { difficulty: newGameDifficulty }
 
     const url = 'https://minesweeper-api.herokuapp.com/games'
 
@@ -35,17 +61,25 @@ export function App() {
     if (response.ok) {
       const newGameStateJSON = await response.json()
 
+      setDifficulty(newGameDifficulty)
       setGame(newGameStateJSON)
+      localStorage.setItem('game-id', newGameStateJSON.id)
+      localStorage.setItem('game-difficulty', String(newGameDifficulty))
     }
   }
 
-  async function handleClickCell(row: number, col: number) {
+  async function handleCheckOrFlagCell(
+    row: number,
+    col: number,
+    action: 'check' | 'flag'
+  ) {
     const checkOptions = {
       id: game.id,
       row,
       col,
     }
-    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/check`
+
+    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/${action}`
     const fetchOptions = {
       method: 'POST',
       headers: { 'content-type': 'application/json' },
@@ -55,29 +89,100 @@ export function App() {
     const response = await fetch(url, fetchOptions)
 
     if (response.ok) {
-      // Get the response as JSON
-      const newGameStateJSON = await response.json()
-      // Make that the new state!
-      setGame(newGameStateJSON)
+      const newGameStateJson = await response.json()
+
+      setGame(newGameStateJson)
+    }
+  }
+
+  function transformCellValue(value: string) {
+    if (value === 'F') {
+      return <i className="fa fa-flag" />
+    }
+
+    if (value === '_') {
+      return ' '
+    }
+
+    if (value === '*') {
+      return <i className="fa fa-bomb" />
+    }
+
+    return value
+  }
+
+  function transformCellClassName(value: string | number) {
+    switch (value) {
+      case 'F':
+        return 'cell-flag'
+
+      case '*':
+        return 'cell-bomb'
+
+      case '_':
+        return 'cell-free'
+
+      case ' ':
+        return undefined
+
+      default:
+        return `cell-number cell-${value}`
     }
   }
 
   return (
-    <div>
+    <main>
       <h1>Minesweeper</h1>
       <p>
         <strong>Instructions:</strong>Choose difficulty level and may the odds
         be ever in your favor!
       </p>
 
-      <label>Difficulty:</label>
+      <h2>
+        <button onClick={() => handleNewGameButtonClick(0)}>
+          New Easy Game
+        </button>
+        <button
+          className="new-game"
+          onClick={() => handleNewGameButtonClick(1)}
+        >
+          New Intermediate Game
+        </button>
+        <button onClick={() => handleNewGameButtonClick(2)}>
+          New Difficult Game
+        </button>
+      </h2>
 
-      <select name="difficulty" id="difficulty">
-        <option value="">--Please choose an option--</option>
-        <option value="easy">Easy</option>
-        <option value="medium">Medium</option>
-        <option value="hard">Hard</option>
-      </select>
-    </div>
+      <p>
+        <h3>Number of Mines: {game.mines}</h3>
+        <h3>Game # {game.id}</h3>
+      </p>
+
+      {/* --------------Cell block---------------- */}
+
+      <section className={`difficulty-${difficulty}`}>
+        {game.board.map(function (gameRow, row) {
+          return gameRow.map(function (square, col) {
+            return (
+              <button
+                className={transformCellClassName(square)}
+                onClick={function (event) {
+                  event.preventDefault()
+                  handleCheckOrFlagCell(row, col, 'check')
+                }}
+                onContextMenu={function (event) {
+                  event.preventDefault()
+
+                  handleCheckOrFlagCell(row, col, 'flag')
+                }}
+                key={col}
+              >
+                {transformCellValue(square)}
+              </button>
+            )
+          })
+        })}
+      </section>
+    </main>
   )
 }
